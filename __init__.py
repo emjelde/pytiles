4f76272c85b9426fd76e9412fd7b599eaa2c17f4
Evan Mjelde <emjelde@gmail.com>
2010-08-13 11:32:30 +0000

    Added project level docstring

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""
+  _______
+ |       |
+ |       |  _______
+ |    ___|_|       |
+ |___|     |       |
+     |     |       |
+     |     |_______|
+     |_______|
+
+ pytiles
+
+@author: Evan Mjelde
+@license: MIT License
+@contact: evan@mjel.de
+"""
--- views/viewtype.py
+++ views/viewtype.py
@@ -3,10 +3,10 @@
 class ViewType(object):
 	"""Supports processing files by passing them and supplied attributes
 	to a special template processor or a custom implementation (format
-	keywords from a dictionary, etc.
+	keywords from a dictionary, etc.)
 	"""
 	def __init__(self):
 		self.attributes = {}
 	
-	def process(self, filepath, attributes):
+	def process(self, resource, attributes):
 		raise NotImplementedError('Should have implemented a view processor')
